4a32f25c92926f948a23c6e75048c04ecc8dd28c
Calvin Ardi <calvin@isi.edu>
2020-09-13 16:28:44 -0700

    hammerspoon: shortcuts for volume {mute, up, down} and debug helpers

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -308,8 +308,173 @@ function dump(o)
    end
 end
 
--- XXX debugg
---print(wifiWatcher)
---print(dump(hs.wifi.interfaceDetails()))
+----
+
+-- hyper sources:
+-- * https://github.com/lodestone/hyper-hacks/blob/master/hammerspoon/init.lua
+
+-- A global variable for the Hyper Mode
+hyper = hs.hotkey.modal.new({}, 'F17')
+
+hyper:bind({}, 'F10', function()
+  output = hs.audiodevice.defaultOutputDevice()
+  output:setMuted(not(output:muted()))
+	hs.alert.closeAll(0.0)
+	if output:muted() then
+		hs.alert.show("Volume: Muted", 0.5)
+	else
+		hs.alert.show("Volume: Unmuted", 0.5)
+	end
+  hyper.triggered = true
+end)
+
+-- need to remap F11 to something like ctrl+shift+alt+cmd-F11
+-- in keyboard shortcuts
+
+function changeVolume(delta)
+	local output = hs.audiodevice.defaultOutputDevice()
+	local volume = math.floor(output:volume() + delta)
+	output:setVolume(volume)
+	hs.alert.closeAll(0.0)
+	hs.alert.show("Volume: " .. math.floor(output:volume()) .. "%", 0.5)
+end
+
+hyper:bind({}, 'F11', function()
+	changeVolume(-10)
+  hyper.triggered = true
+end)
+
+-- https://gist.github.com/spinscale/fd82f00da29447990f27f36b3f4b927d
+hyper:bind({}, 'F12', function()
+	changeVolume(10)
+  hyper.triggered = true
+end)
+
+----
+
+-- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
+function enterHyperMode()
+  hyper.triggered = false
+  hyper:enter()
+end
+
+-- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
+-- send ESCAPE if no other keys are pressed.
+function exitHyperMode()
+  hyper:exit()
+  if not hyper.triggered then
+    hs.eventtap.keyStroke({}, 'ESCAPE')
+  end
+end
+
+-- Bind the Hyper key (to context menu)
+f18 = hs.hotkey.bind({}, 110, enterHyperMode, exitHyperMode)
+
+----
+
+-- "Show Key Press Mode": this is helpful when trying to figure out which
+-- keycode corresponds to the physical key you press. I used this to figure out
+-- the keycode for the "Menu" key and assign it as a "hyper key" (like Ctrl,
+-- Shift, Alt, Cmd, etc.).
+--
+-- Source: https://kirbuchi.com/2016/10/26/playing-with-hammerspoon/
+function showKeyPress(tap_event)
+  local duration = 1.5  -- popup duration
+  local modifiers = ""  -- key modifiers string representation
+  local flags = tap_event:getFlags()
+  local character = hs.keycodes.map[tap_event:getKeyCode()]
+  -- we only want to read special characters via getKeyCode, so we
+  -- use this subset of hs.keycodes.map
+  local special_chars = {
+    ["f1"] = true, ["f2"] = true, ["f3"] = true, ["f4"] = true,
+    ["f5"] = true, ["f6"] = true, ["f7"] = true, ["f8"] = true,
+    ["f9"] = true, ["f10"] = true, ["f11"] = true, ["f12"] = true,
+    ["f13"] = true, ["f14"] = true, ["f15"] = true, ["f16"] = true,
+    ["f17"] = true, ["f18"] = true, ["f19"] = true, ["f20"] = true,
+    ["pad"] = true, ["pad*"] = true, ["pad+"] = true, ["pad/"] = true,
+    ["pad-"] = true, ["pad="] = true, ["pad0"] = true, ["pad1"] = true,
+    ["pad2"] = true, ["pad3"] = true, ["pad4"] = true, ["pad5"] = true,
+    ["pad6"] = true, ["pad7"] = true, ["pad8"] = true, ["pad9"] = true,
+    ["padclear"] = true, ["padenter"] = true, ["return"] = true,
+    ["tab"] = true, ["space"] = true, ["delete"] = true, ["escape"] = true,
+    ["help"] = true, ["home"] = true, ["pageup"] = true,
+    ["forwarddelete"] = true, ["end"] = true, ["pagedown"] = true,
+    ["left"] = true, ["right"] = true, ["down"] = true, ["up"] = true
+  }
+
+  -- if we have a simple character (no modifiers), we want a shorter
+  -- popup duration.
+  if (not flags.shift and not flags.cmd and
+        not flags.alt and not flags.ctrl) then
+    duration = 0.3
+  end
+
+  -- we want to get regular characters via getCharacters as it
+  -- "cleans" the key for us (e.g. for a "⇧-5" keypress we want
+  -- to show "⇧-%").
+  if special_chars[character] == nil then
+    character = tap_event:getCharacters(true)
+    if flags.shift then
+      character = string.lower(character)
+    end
+  end
+
+  -- make some known special characters look good
+  if character == "return" then
+    character = "⏎"
+  elseif character == "delete" then
+    character = "⌫"
+  elseif character == "escape" then
+    character = "⎋"
+  elseif character == "space" then
+    character = "SPC"
+  elseif character == "up" then
+    character = "↑"
+  elseif character == "down" then
+    character = "↓"
+  elseif character == "left" then
+    character = "←"
+  elseif character == "right" then
+    character = "→"
+  end
+
+  -- get modifiers' string representation
+  if flags.ctrl then
+    modifiers = modifiers .. "C-"
+  end
+  if flags.cmd then
+    modifiers = modifiers .. "⌘-"
+  end
+  if flags.shift then
+    modifiers = modifiers .. "⇧-"
+  end
+  if flags.alt then
+    modifiers = modifiers .. "⌥-"
+  end
+
+  -- actually show the popup
+  hs.alert.show(modifiers .. character, duration)
+
+end
+
+
+local key_tap = hs.eventtap.new(
+  {hs.eventtap.event.types.keyDown},
+  showKeyPress
+)
+
+-- Enable/Disable Keypress Show Mode with "C-⌘-⇧-p"
+k = hs.hotkey.modal.new({"cmd", "shift", "ctrl"}, 'P')
+function k:entered()
+  hs.alert.show("Enabling Keypress Show Mode", 1.5)
+  key_tap:start()
+end
+function k:exited()
+  hs.alert.show("Disabling Keypress Show Mode", 1.5)
+end
+k:bind({"cmd", "shift", "ctrl"}, 'P', function()
+    key_tap:stop()
+    k:exit()
+end)
 
 -- vim: set expandtab ts=2 sw=2:
