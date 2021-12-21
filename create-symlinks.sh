f9a626d6579ca262eff21e23abaa4540b640ed12
Calvin Ardi <calvin@isi.edu>
2021-12-20 21:24:34 -0800

    create-symlinks.sh: update TODO, force create symlinks

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -3,12 +3,14 @@
 # requires port:coreutils
 DOTFILES=$(dirname $(greadlink -f $0))
 
-# TODO if the dotfiles exist, move them to a backup location
-ln -s $DOTFILES/git/gitconfig        ~/.gitconfig
-ln -s $DOTFILES/git/gitignore_global ~/.gitignore_global
-ln -s $DOTFILES/hammerspoon          ~/.hammerspoon
-ln -s $DOTFILES/screen/screenrc      ~/.screenrc
-ln -s $DOTFILES/vim/vimrc            ~/.vimrc
+# TODO if the dotfiles or dotfiles folder already exists, move them to a
+# backup location, otherwise it creates a symlink inside the existing
+# folder.
+ln -fs $DOTFILES/git/gitconfig        ~/.gitconfig
+ln -fs $DOTFILES/git/gitignore_global ~/.gitignore_global
+ln -fs $DOTFILES/hammerspoon          ~/.hammerspoon
+ln -fs $DOTFILES/screen/screenrc      ~/.screenrc
+ln -fs $DOTFILES/vim/vimrc            ~/.vimrc
 
-ln -s $DOTFILES/zsh/zshrc            ~/.zshrc
-ln -s $DOTFILES/zsh/zshenv           ~/.zshenv
+ln -fs $DOTFILES/zsh/zshrc            ~/.zshrc
+ln -fs $DOTFILES/zsh/zshenv           ~/.zshenv
